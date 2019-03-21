a88b13943d6fe2aca54f2e91e5b9b38b0b30ec38
Harkame <louisr.daviaud@gmail.com>
2019-03-21 13:57:49 +0100

    complete setup

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
 from setuptools import setup, find_packages
 
-setup(name='japscandownloader', version='1.0', packages=find_packages())
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="japscandownloader",
+    version="0.0.1",
+    author="Louis Daviaud",
+    description="Program to download mangas from JapScan",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Harkame/JapScanDownloader",
+    packages=find_packages(),
+    classifiers=[
+        "Manga downloader",
+    ],
+)
