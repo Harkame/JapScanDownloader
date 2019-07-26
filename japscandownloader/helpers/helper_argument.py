dbdab14dda44cb9d35dd800ad0edd30074ed60bb
Harkame <louisr.daviaud@gmail.com>
2019-07-26 14:09:14 +0200

    start using fstring

--- japscandownloader/helpers/helper_argument.py
+++ japscandownloader/helpers/helper_argument.py
@@ -7,43 +7,53 @@ def get_arguments(arguments):
 
     argument_parser.add_argument(
         '-c', '--config_file',
-        help = 'Set config file' + os.linesep + 'Example : python japscandownloader/main.py -c /home/myconfigfile.yml',
+        help = f"Set config file"
+                "{os.linesep}"
+                "Example : python japscandownloader/main.py -c /home/myconfigfile.yml",
         type = str
     )
 
     argument_parser.add_argument(
         '-d', '--destination_path',
-        help = 'Set destination path of downloaded mangas' + os.linesep + 'Example : python japscandownloader/main.py -d /home/mymangas/',
+        help = f"Set destination path of downloaded mangas"
+                "Example : python japscandownloader/main.py -d /home/mymangas/",
         type = str,
     )
 
     argument_parser.add_argument(
         '-f', '--format',
-        help = 'Set format of downloaded mangas' + os.linesep + 'Example : python japscandownloader/main.py -f cbz|pdf|jpg|png',
+        help = f"Set format of downloaded mangas"
+                "Example : python japscandownloader/main.py -f cbz|pdf|jpg|png",
         type = str,
     )
 
     argument_parser.add_argument(
         '-v', '--verbose',
-        help = 'Active verbose mode, support different level' + os.linesep + 'Example : python japscandownloader/main.py -vv',
+        help = f"Active verbose mode, support different level"
+                "Example : python japscandownloader/main.py -vv",
         action = 'count',
     )
 
     argument_parser.add_argument(
         '-r', '--reverse',
-        help = 'Reverse chapters download order' + os.linesep + 'Default : Last to first' + os.linesep + 'Example : python japscandownloader/main.py -r',
+        help = f"Reverse chapters download order"
+                "Default : Last to first"
+                "Example : python japscandownloader/main.py -r",
         action = 'count',
     )
 
     argument_parser.add_argument(
         '-k', '--keep',
-        help = 'Keep downloaded images (when format is pdf/cbz)' + os.linesep + 'Default : false' + os.linesep + 'Example : python japscandownloader/main.py -k',
+        help = f"Keep downloaded images (when format is pdf/cbz)"
+                "Default : false"
+                "Example : python japscandownloader/main.py -k",
         action = 'count',
     )
 
     argument_parser.add_argument(
         '-u', '--unscramble',
-        help = 'Force unscrambling' + os.linesep + 'Example : python japscandownloader/main.py -u',
+        help = "Force unscrambling"
+                "Example : python japscandownloader/main.py -u",
         action = 'count',
     )
 
